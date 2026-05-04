d9d41ee02a54dbbcac966f1dbbbc7bac07a141f0
adsrmk <74286623+adsrmk@users.noreply.github.com>
2026-05-05 00:20:02 +0200

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,30 +1,5 @@
-import DefaultTheme from 'vitepress/theme'
-import type { Theme } from 'vitepress'
+import Theme from 'vitepress/theme'
+// import 'virtual:group-icons.css'
 import './styles.css'
 
-const isDeveloperRoute = (path: string) => path.includes('/developers/')
-
-const applyThemeForRoute = (path: string) => {
-  if (typeof document === 'undefined') return
-  const html = document.documentElement
-  if (isDeveloperRoute(path)) {
-    html.classList.add('dark')
-  } else {
-    html.classList.remove('dark')
-  }
-}
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ router }) {
-    if (typeof window === 'undefined') return
-
-    // Initial load - meteen toepassen
-    applyThemeForRoute(window.location.pathname)
-
-    // Bij navigatie
-    router.onBeforeRouteChange = (to) => {
-      applyThemeForRoute(to)
-    }
-  }
-} satisfies Theme
+export default Theme
